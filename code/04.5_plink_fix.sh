529acf0ddc33fff47394a67966b6b92e9fd1fdaa
ljfabian <ljfabian97@gmail.com>
2026-06-18 15:31:44 +0100

    Allow no plink fix to be ran if it already has #IID FID rather than just #FID

--- code/04.5_plink_fix.sh
+++ code/04.5_plink_fix.sh
@@ -12,21 +12,42 @@ temp_path=$1
 filtered_prefix=$2
 gwas_prefix=$3
 
-# fix only #iid to iid fid with plink2
-sed '1s/^#IID/#FID\tIID/' $temp_path/$filtered_prefix.psam | \
- awk 'NR>1{print $1 "\t" $0; next}1' \
- > $temp_path/pf_$filtered_prefix.psam
 
+# run for subset version, which is used in regenie step 1
+if head -1 "$temp_path/$filtered_prefix.psam" | grep -q '^#FID'; then
+    
+    # already has FID + IID and leave as is
+    cp $temp_path/$filtered_prefix.psam $temp_path/pf_$filtered_prefix.psam
+
+else
+    
+    # only #IID present, fix only #iid to iid fid with plink2
+    sed '1s/^#IID/#FID\tIID/' $temp_path/$filtered_prefix.psam | \
+    awk 'NR>1{print $1 "\t" $0; next}1' \
+    > $temp_path/pf_$filtered_prefix.psam
+fi
+# copy pvar and pgen
 cp $temp_path/$filtered_prefix.pvar $temp_path/pf_$filtered_prefix.pvar
 cp $temp_path/$filtered_prefix.pgen $temp_path/pf_$filtered_prefix.pgen
 
-sed '1s/^#IID/#FID\tIID/' $temp_path/$gwas_prefix.psam | \
- awk 'NR>1{print $1 "\t" $0; next}1' \
- > $temp_path/pf_$gwas_prefix.psam
+# run for full version, used in regenie step 2
+if head -1 "$temp_path/$gwas_prefix.psam" | grep -q '^#FID'; then
 
+    # already has FID _ IID and leave as is
+    cp $temp_path/$gwas_prefix.psam $temp_path/pf_$gwas_prefix.psam
+
+else
+
+    sed '1s/^#IID/#FID\tIID/' $temp_path/$gwas_prefix.psam | \
+    awk 'NR>1{print $1 "\t" $0; next}1' \
+    > $temp_path/pf_$gwas_prefix.psam
+
+fi
+# copy across pvar and pgen
 cp $temp_path/$gwas_prefix.pvar $temp_path/pf_$gwas_prefix.pvar
 cp $temp_path/$gwas_prefix.pgen $temp_path/pf_$gwas_prefix.pgen
 
+#list all out to check they're present
 ls $temp_path/pf*
 #   plink2 \
 #     --pfile $temp_path/$gwas_prefix \
